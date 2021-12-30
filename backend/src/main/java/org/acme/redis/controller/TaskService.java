64a2f0817d6fb8e0443749dbb64f1ab4032e5982
João Pedro Cruz <jpcgomes0@gmail.com>
2021-12-30 02:59:19 -0300

    Backend - Finalizado

---
+++ backend/src/main/java/org/acme/redis/controller/TaskService.java
@@ -0,0 +1,48 @@
+package org.acme.redis.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import io.quarkus.redis.client.RedisClient;
+import io.quarkus.redis.client.reactive.ReactiveRedisClient;
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.redis.client.Response;
+
+@Singleton
+public class TaskService {
+
+	@Inject
+	RedisClient redisClient;
+
+	@Inject
+	ReactiveRedisClient reactiveRedisClient;
+
+	public Uni<List<String>> keys() {
+		return reactiveRedisClient.keys("*").map(response -> {
+			List<String> result = new ArrayList<>();
+			for (Response r : response) {
+				result.add(r.toString());
+			}
+			return result;
+		});
+	}
+
+	public String get(String key) {
+		return redisClient.get(key).toString();
+	}
+
+	public void set(String key, String value) {
+		if (value.length() >= 10) {
+			redisClient.set(Arrays.asList(key, value));
+		}
+	}
+
+	public Uni<Void> del(String key) {
+		return reactiveRedisClient.del(Arrays.asList(key)).map(response -> null);
+	}
+
+}
